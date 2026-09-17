9a5f8bb508e4945dffc21b9ad2b76f2a7f9efc8b
geronimo <geronimo@geronimo22.ynh.fr>
2026-09-17 14:10:49 +0200

    fix: use_reloader=False (casse Python)

--- app.py
+++ app.py
@@ -475,4 +475,4 @@ if __name__ == '__main__':
     if not os.environ.get("WERKZEUG_RUN_MAIN"):
         threading.Timer(1.0, open_browser).start()
 
-    app.run(host='127.0.0.1', port=5000, debug=debug_mode, use_reloader=false)
+    app.run(host='127.0.0.1', port=5000, debug=debug_mode, use_reloader=False)
